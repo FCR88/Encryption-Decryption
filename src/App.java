6c584dbb8a5b8562e02eb0b1dbce5711ad1bc452
Florea Cristian <dev@n7.ro>
2019-11-13 14:12:06 +0200

    Add an encrypt method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("HW!");
+//        String textToEncrypt = "welcome to hyperskill";
+//        int key = 5;
+        //output of encrypted text: bjqhtrj yt mdujwxpnqq
+
+        Scanner sc = new Scanner(System.in);
+
+        String textToEncrypt = sc.nextLine();
+        int key = sc.nextInt();
+
+        String encryptedText = encryptString(textToEncrypt, key);
+
+        System.out.println(textToEncrypt);
+        System.out.println(encryptedText);
+    }
+
+    static String encryptString(String textToEncrypt, int key) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // if key is 5 encoded alphabet looks like this: "fghijklmnopqrstuvwxyzabcde"
+        String encodedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
+
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int idx = 0; idx < textToEncrypt.length(); idx++) {
+            char ch = textToEncrypt.charAt(idx);
+            char encodedChar;
+
+            //we check for other characters besides those in alphabet and
+            // if found add them directly as they are to the encoded string:
+            if (alphabet.contains(String.valueOf(ch))) {
+                int charAlphabetIdx = alphabet.indexOf(ch);
+                encodedChar = encodedAlphabet.charAt(charAlphabetIdx);
+            } else {
+                encodedChar = ch;
+            }
+            encryptedText.append(encodedChar);
+        }
+        return String.valueOf(encryptedText);
     }
 }
